ba83d8a67f489d764b401b58b02d47e57cf7c3fa
Phat Tran <ptran@strongtie.com>
2024-02-08 10:44:10 +0700

    Finish

--- JimmyLinq/ComicAnalyzer.cs
+++ JimmyLinq/ComicAnalyzer.cs
@@ -8,5 +8,36 @@ namespace JimmyLinq
 {
     class ComicAnalyzer
     {
+        public static IEnumerable<string> GetReviews(IEnumerable<Comic> catalog, IEnumerable<Review> reviews)
+        {
+            var reviewed = from comic in catalog
+                           orderby comic.Issue
+                           join review in reviews on comic.Issue equals review.Issue
+                           select $"{review.Critic} rated #{comic.Issue} '{comic.Name}' {review.Score:0.00}";
+
+            return reviewed;
+        }
+
+        public static IEnumerable<IGrouping<PriceRange, Comic>> GroupComicsByPrice(IEnumerable<Comic> catalog, IReadOnlyDictionary<int, decimal> prices)
+        {
+            var grouped = from comic in catalog
+                          orderby prices[comic.Issue]
+                          group comic by CalculatePriceRange(comic) into groups
+                          select groups;
+
+            return grouped;
+        }
+
+        private static PriceRange CalculatePriceRange(Comic comic)
+        {
+            if (Comic.Prices[comic.Issue] > 100M)
+            {
+                return PriceRange.Expensive;
+            }
+            else
+            {
+                return PriceRange.Cheap;
+            }
+        }
     }
 }
