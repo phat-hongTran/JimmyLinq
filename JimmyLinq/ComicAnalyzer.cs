775a8b47e4a34597904edd8d61a437d25d0b0768
Phat Tran <ptran@strongtie.com>
2024-02-13 05:27:07 +0700

    Refactor

--- JimmyLinq/ComicAnalyzer.cs
+++ JimmyLinq/ComicAnalyzer.cs
@@ -10,20 +10,25 @@ namespace JimmyLinq
     {
         public static IEnumerable<string> GetReviews(IEnumerable<Comic> catalog, IEnumerable<Review> reviews)
         {
-            var reviewed = from comic in catalog
-                           orderby comic.Issue
-                           join review in reviews on comic.Issue equals review.Issue
-                           select $"{review.Critic} rated #{comic.Issue} '{comic.Name}' {review.Score:0.00}";
+            //var reviewed = from comic in catalog
+            //               orderby comic.Issue
+            //               join review in reviews on comic.Issue equals review.Issue
+            //               select $"{review.Critic} rated #{comic.Issue} '{comic.Name}' {review.Score:0.00}";
+
+            var reviewed = catalog.OrderBy(x => x.Issue).
+                Join(reviews, x => x.Issue, y => y.Issue, (x, y) => $"{y.Critic} rated #{x.Issue} '{x.Name}' {y.Score:0.00}");
 
             return reviewed;
         }
 
         public static IEnumerable<IGrouping<PriceRange, Comic>> GroupComicsByPrice(IEnumerable<Comic> catalog, IReadOnlyDictionary<int, decimal> prices)
         {
-            var grouped = from comic in catalog
-                          orderby prices[comic.Issue]
-                          group comic by CalculatePriceRange(comic, prices) into groups
-                          select groups;
+            //var grouped = from comic in catalog
+            //              orderby prices[comic.Issue]
+            //              group comic by CalculatePriceRange(comic, prices) into groups
+            //              select groups;
+
+            var grouped = catalog.OrderBy(x => prices[x.Issue]).GroupBy(x => CalculatePriceRange(x, prices));
 
             return grouped;
         }
